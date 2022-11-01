f5e81897a3b54efcd2ad6d292c7a6c1edfe3e478
Chintan Acharya <chintan.acharya27@gmail.com>
2022-10-31 21:41:09 -0400

    Edited Colorize.py

--- colorize.py
+++ colorize.py
@@ -3,35 +3,39 @@ import tensorflow as tf
 import cv2
 import sys
 
+
 def testModel(modelpath, modelNo, imagepath):
-  model = tf.keras.models.load_model(modelpath)
-  img = cv2.imread(imagepath)
-  img = cv2.resize(img, (128, 128), interpolation = cv2.INTER_AREA)
-  imgLAB = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-  L, a, b = cv2.split(imgLAB)
-  L = L / 255
-  L = L.reshape(1, 1, 128, 128)
+    print(modelpath)
+    model = tf.keras.models.load_model(modelpath)
+    img = cv2.imread(imagepath)
+    img = cv2.resize(img, (128, 128), interpolation=cv2.INTER_AREA)
+    imgLAB = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    L, a, b = cv2.split(imgLAB)
+    L = L / 255
+    L = L.reshape(1, 1, 128, 128)
 
-  prediction = model.predict(L)
-  a = prediction[0][0]
-  b = prediction[0][1]
-  L = L.reshape(128,128)
-  L = np.array(L * 255, dtype = np.uint8)
-  if(modelNo == 0):
-    a = np.array(a * 255, dtype = np.uint8)
-    b = np.array(b * 255, dtype = np.uint8)  
-  elif(modelNo == 1):
-    a = np.array(a * 51, dtype = np.uint8)
-    b = np.array(b * 51, dtype = np.uint8)
-    a = a + 144
-    b = b + 144
-  elif(modelNo == 2):
-    a = np.array(a * 127, dtype = np.uint8)
-    b = np.array(b * 127, dtype = np.uint8)
-    a = a + 128
-    b = b + 128
-  imgLAB = cv2.merge([L, a, b])
-  image = cv2.cvtColor(imgLAB, cv2.COLOR_LAB2BGR)
-  cv2.imwrite('result.jpg', image)
+    prediction = model.predict(L)
+    a = prediction[0][0]
+    b = prediction[0][1]
+    L = L.reshape(128, 128)
+    L = np.array(L * 255, dtype=np.uint8)
+    if(modelNo == 0):
+        a = np.array(a * 255, dtype=np.uint8)
+        b = np.array(b * 255, dtype=np.uint8)
+    elif(modelNo == 1):
+        a = np.array(a * 51, dtype=np.uint8)
+        b = np.array(b * 51, dtype=np.uint8)
+        a = a + 144
+        b = b + 144
+    elif(modelNo == 2):
+        a = np.array(a * 127, dtype=np.uint8)
+        b = np.array(b * 127, dtype=np.uint8)
+        a = a + 128
+        b = b + 128
+    print(b.shape, L.shape)
+    imgLAB = cv2.merge([L, a, b])
+    image = cv2.cvtColor(imgLAB, cv2.COLOR_LAB2BGR)
+    cv2.imwrite('result.jpg', image)
 
-testModel(sys.argv[0], sys.argv[2], sys.argv[1])
+
+testModel(sys.argv[1], int(sys.argv[3]), sys.argv[2])
